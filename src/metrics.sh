836970e8543814e9502e67d5c43a0f6fab0f203c
Raffa064 <raffaddtg@gmail.com>
2024-11-17 12:42:17 -0300

    Added colors to the output

--- src/metrics.sh
+++ src/metrics.sh
@@ -1,6 +1,14 @@
 import colorful
 
 function metrics/main() {
+  metrics/file-metrics $@
+  
+  if git branch >/dev/null 2>&1; then
+    metrics/git-metrics
+  fi
+}
+
+function metrics/file-metrics() {
   local path
   local regex=".*"
   local file_ext="*"
@@ -28,14 +36,14 @@ function metrics/main() {
 
   local find_command="find $path -regex '$regex' -type f -name '*.$file_ext'$exclude_exp | xargs wc"
 
-  echo "File metrics:"
-
   local wcArgs=( 
     "-l lines"
     "-w words"
     "-m chars"
     "-c bytes"
   )
+
+  echoe "${fy}File metrics"
 
   local arg
   for arg in "${wcArgs[@]}"; do
@@ -46,22 +54,19 @@ function metrics/main() {
 
     local result=$(eval "$find_command $arg_flag" | grep total)
     result=$(sed "s/total/$arg_name/" <<< "$result")
-    echo "  * $result"
+    echoe "  $fg*$Sr $result"
   done
 
-  if git branch >/dev/null 2>&1; then
-    metrics/git-metrics
-  fi
 }
 
 function metrics/git-metrics() {
-  echo "Git metrics:"
+  echoe "${fr}Git metrics:"
 
   local commit_count=$(git log --pretty=%ad | wc -l)
   local branch_count=$(git branch | wc -l)
   local work_days=$(git log --pretty=%ad --date=format:%Y-%m-%d | uniq | wc -l)
 
-  echo "  * $commit_count commits"
-  echo "  * $branch_count branches"
-  echo "  * $work_days days worked"
+  echoe "  $fg*$Sr $commit_count commits"
+  echoe "  $fg*$Sr $branch_count branches"
+  echoe "  $fg*$Sr $work_days days worked"
 }
